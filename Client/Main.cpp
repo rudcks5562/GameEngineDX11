e008672e01eb07638a8f69de4df46586da646455
rudcks5562 <rudcks5562@naver.com>
2025-02-12 16:21:48 +0900

    [mod] depth stencil added

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -2,7 +2,7 @@
 #include "Main.h"
 #include "Engine/Game.h"
 
-#include "GlobalTestDemo.h"
+#include "DepthStencilDemo.h"
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
@@ -14,7 +14,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.5f, 0.5f, 0.5f, 0.5f);
-	desc.app = make_shared<GlobalTestDemo>();
+	desc.app = make_shared<DepthStencilDemo>();
 
 	GAME->Run(desc);
 
